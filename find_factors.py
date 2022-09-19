20eb9a96023fc353176e47bae32d5c8e31eaddee
Daniel Igwe <daniel.igwe000@gmail.com>
2022-09-19 00:09:08 +0000

    Improving the algorithm

--- find_factors.py
+++ find_factors.py
@@ -2,18 +2,14 @@
 def FindFactors(n):
     try:
         #declare variables
-        p = 0
-        q = 0
         i = 0
         factors = []
 
         for i in range(2, n):
             if (n % i) == 0:
-                p = i
-                q = int(n / i)
+                factors.append(i)
+                factors.append(int(n / i))
                 break
-        factors.append(p)
-        factors.append(q)
     except Exception as e:
         print("{:s}: an error occurred".format(e))
     finally:
@@ -22,20 +18,16 @@ def FindFactors(n):
 def find_prime_factors(n):
     try:
         #declare variables
-        p = 0
-        q = 0
         i = 0
         factors = []
 
-        for i in range(2, n):
+        for i in range(3, n, 2):
             if ((n % i == 0) and i % 2 == 0 or i % 3 == 0 and i > 7):
                 continue
             elif (n % i) == 0:
-                p = i
-                q = int(n / i)
+                factors.append(i)
+                factors.append(int(n / i))
                 break
-        factors.append(p)
-        factors.append(q)
     except Exception as e:
         print("{:s}: an error occurred".format(e))
     finally:
